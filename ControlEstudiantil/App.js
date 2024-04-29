eba66d14ceb1ed03b273c61c22ef24c0f8051962
ZANK123-F <frankmarroquin.dev@gmail.com>
2024-04-29 13:18:07 -0600

    subiendo login

--- ControlEstudiantil/App.js
+++ ControlEstudiantil/App.js
@@ -1,10 +1,17 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { StatusBar, Image, TextInput, TouchableOpacity, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your oki vamos a empezar ya!</Text>
+      <Image source={require('./assets/login.png')} style={styles.image} />
+      <View style={styles.formContainer}>
+        <TextInput style={styles.input} placeholder="Usuario" />
+        <TextInput style={styles.input} placeholder="Contraseña" secureTextEntry={true} />
+        <TouchableOpacity style={styles.button}>
+          <Text style={styles.buttonText}>Iniciar Sesión</Text>
+        </TouchableOpacity>
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +20,37 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#6B3ABB',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'flex-end',
+  },
+  image: {
+    width: '100%',
+    height: '60%',
+    resizeMode: 'contain',
+  },
+  formContainer: {
+    width: '80%',
+    marginTop: 20,
+    marginBottom: 50,
+  },
+  input: {
+    height: 40,
+    borderColor: '#311A55',
+    borderWidth: 1,
+    borderRadius: 5,
+    paddingHorizontal: 10,
+    marginBottom: 10,
+    color: '#fff',
+  },
+  button: {
+    backgroundColor: '#311A55',
+    padding: 10,
+    borderRadius: 5,
+    alignItems: 'center',
+  },
+  buttonText: {
+    color: '#fff',
+    fontSize: 16,
   },
 });
